4029feee0388db40c6d2817da57228f5d22cbf86
Jorge Felix <desarrollo2@motidigital.com>
2018-07-06 18:03:05 -0500

    Nueva seccion de agrega comentario

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express      = require("express"),
     bodyParser   = require("body-parser"),
     mongoose     = require("mongoose"),
     Campground   = require("./models/campground"),
+    Comment      = require("./models/comment"),
     seedDB       = require("./seeds");
 
 seedDB();
@@ -78,9 +79,27 @@ app.get("/campgrounds/:id/comments/new", function(req,res){
         }else{
             res.render("comments/new", {campground: campground});            
         }
-    })
+    });
+});
 
-})
+app.post("/campgrounds/:id/comments", function(req,res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+        }
+    })
+});
 
 app.listen(3000, function(){
     console.log("The YelpCamp has started");
